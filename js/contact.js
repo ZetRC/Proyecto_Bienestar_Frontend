5913854623a2e17dc4541949777e781e3e329601
ZetRC <zetrosas555@gmail.com>
2022-05-21 10:43:11 -0500

    fixed map errors

--- js/contact.js
+++ js/contact.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
     console.log("cmon yo")
       var mymap = L.map('mapid').setView([29.79, -98.42], 13);
   
+  
       L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
@@ -13,7 +14,12 @@ $(document).ready(function(){
         accessToken: 'pk.eyJ1IjoiaG9odXNlcjEyMyIsImEiOiJja25ldHM1Y2YwMHl0MnhwYTFjcmhrMjd1In0.oWaWYfJfKI3HsQrmx-huDA'
       }).addTo(mymap);
   
-      var marker = L.marker([29.79, -98.42]).addTo(mymap);
+
+      const cordX = 39.546517
+      const cordY= -0.577849
+      mymap.panTo(new L.LatLng(cordX, cordY));
+      mymap.setZoom(10)
+      var marker = L.marker([cordX, cordY]).addTo(mymap);
       marker.bindPopup("Centro Armonium -Villamarchante - Valencia").openPopup();
     }
     
