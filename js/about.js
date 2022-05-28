8cf1d57c4b12611775335946bae3eadb3dc1be76
ZetRC <zetrosas555@gmail.com>
2022-05-28 12:18:09 -0500

    cambios en layout y estilos

---
+++ js/about.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    const runYoutubeVideo = () =>{
+      $('.yt-btn').click((e)=>{
+        var myModal = new bootstrap.Modal(document.getElementById('videosModal'))
+        myModal.show()
+        var videoId = e.target.getAttribute("data-youtube");
+        $('#youtube-video').html(
+          `<iframe id="yt-video" width="1100" height="700" src="https://www.youtube.com/embed/${videoId}?wmode=transparent" frameborder="0" allowfullscreen wmode="Opaque"></iframe>`
+        )
+      })
+
+      var myModalEl = document.getElementById('videosModal')
+      myModalEl.addEventListener('hidden.bs.modal', function (event) {
+        $('#yt-video').attr("src", " ");
+        $('#yt-video').remove();
+      })
+    }
+    
+    runYoutubeVideo()
+  });
