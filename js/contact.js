cfa5bccbf9327c5811fefc03b709d9dbee2d8014
ZetRC <zetrosas555@gmail.com>
2022-06-11 12:11:49 -0500

    links añadidos y detalles en layout arreglados

--- js/contact.js
+++ js/contact.js
@@ -1,9 +1,7 @@
 $(document).ready(function(){
 
     function initMap() {
-    console.log("cmon yo")
-      var mymap = L.map('mapid').setView([29.79, -98.42], 13);
-  
+      var mymap = L.map('mapid').setView([29.79, -98.42], 13);  
   
       L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
